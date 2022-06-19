807b316e805fdc9a57f086b7e1ffea0f5bd26bbb
catalinito <tomascatalini@gmail.com>
2022-06-19 18:15:09 -0300

    Retoques Formulario y Limpieza

--- ui/src/app/shared/components/form/form.component.ts
+++ ui/src/app/shared/components/form/form.component.ts
@@ -3,27 +3,41 @@ import { FormGroup } from '@angular/forms';
 
 import { Field } from '../../model/Field';
 import { FieldService } from '../../services/field.service';
+import { GenericService } from '../../services/generic.service';
 
 @Component({
-  selector: 'app-form',
+  selector: 'generic-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
 
-  @Input() entity!: string;
+  @Input() entity: string;
   @Input() fields!: Field[] | null;
   @Input() data!: any;
   
   formGroup: FormGroup;
 
   constructor( 
-    private fieldService: FieldService
+    private fieldService: FieldService,
+    private service: GenericService<any>
     ) {}
 
   ngOnInit(): void {
 
-    //Get form fields
+    //Get data
+    if( !this.fields ){
+      this.service.findAll().subscribe({ 
+          next: (res) => this.fields = res,
+          error: (e) => { 
+            console.warn('Error al obtener datos para el formulario');
+            console.log(e)
+          },
+          complete: () => console.info('complete')
+      });
+    }
+
+    //Transform feilds to FormGroup
     this.formGroup = this.fieldService.toFormGroup( this.fields as Field[] );
     
     if(this.data){
