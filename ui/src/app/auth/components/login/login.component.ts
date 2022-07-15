c6ada731059b5031c4d4869bae8107fc2c0d6daa
catalinito <tomascatalini@gmail.com>
2022-07-15 17:53:50 -0300

    pegar back desde login

--- ui/src/app/auth/components/login/login.component.ts
+++ ui/src/app/auth/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,18 +11,25 @@ export class LoginComponent {
 
   formulario: FormGroup = this.fb.group(
     {
-      user: ['', [Validators.required, Validators.minLength(5)]],
-      pass: ['', [Validators.required, Validators.minLength(6)]]
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     }
   );
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private authService: AuthService
   ) { }
 
   login() {
     console.log(this.formulario.value);
-    console.log(this.formulario.valid);
+    // console.log(this.formulario.valid);
+    const {username, password} = this.formulario.value;
+
+    this.authService.login( username, password )
+      .subscribe( resp => {
+        console.log(resp);
+      });
   }
 
 }
