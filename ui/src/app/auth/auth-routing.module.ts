cc168f6701ff08df431c25f2e0a7f94586cc806c
catalinito <tomascatalini@gmail.com>
2022-07-15 17:44:12 -0300

    minor changes

--- ui/src/app/auth/auth-routing.module.ts
+++ ui/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
